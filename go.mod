a4db8391fdd0e1f2b8eaf110ae385c00c6000e70
Franz Dalitz <80651545+Franz-Dalitz@users.noreply.github.com>
2024-02-14 20:53:12 +0100

    dirty inbetween push refactor

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/franz-dalitz/api-debugger
+
+go 1.21.7
--- web/ui/tailwind.config.js
+++ web/ui/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.html"],
+  content: ["./templates/**/*.html"],
   theme: {
     extend: {},
   },
