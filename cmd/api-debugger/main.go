e78315d97f0589f51d2eea62f020f0af5e1f2ca8
Franz Dalitz <80651545+Franz-Dalitz@users.noreply.github.com>
2024-02-19 13:00:23 +0100

    add logfmt logging with slog

--- cmd/api-debugger/main.go
+++ cmd/api-debugger/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
+	"log/slog"
+	"net/http"
+	"os"
 
-	"github.com/franz-dalitz/api-debugger/yaml"
+	"github.com/franz-dalitz/api-debugger/config"
+	"github.com/franz-dalitz/api-debugger/routes"
 )
 
 func main() {
-	config, err := yaml.LoadConfig("examples/config.yaml")
+	// customize slog to use logfmt
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	// load and log configuration
+	cfg, err := config.LoadConfig("examples/config.yaml")
 	if err != nil {
+		slog.Error("failed to read config file")
 		log.Fatal(err)
 	}
-	bytes, _ := json.MarshalIndent(config, "", "  ")
-	fmt.Println(string(bytes))
+	slog.Info(fmt.Sprintf("successfully loaded the following config: %v", cfg))
+
+	// set up routes and start server
+	mux := http.NewServeMux()
+	err = routes.RegisterRoutes(mux)
+	if err != nil {
+		slog.Error("failed to register routes")
+		log.Fatal(err)
+	}
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		slog.Error("http handler failure")
+		log.Fatal(err)
+	}
 }
---
+++ routes/routes.go
@@ -0,0 +1,9 @@
+package routes
+
+import (
+	"net/http"
+)
+
+func RegisterRoutes(mux *http.ServeMux) error {
+	return nil
+}
